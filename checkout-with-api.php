fa61a5f46e6c8f41db92a7357bf3c1e3fd7c943f
Krishna <krishnaitbpl@gmail.com>
2019-03-19 17:10:55 +0530

    Example files added

---
+++ checkout-with-api.php
@@ -0,0 +1,45 @@
+<?php
+
+//Subscription plan id
+$plan_id = "Put your Pabbly Subscription plan id here";
+
+//Api credential of Pabbly Subscription
+$apiKey = ""; //Put your api key here
+$apiSecret = ""; // Put your api secret here
+$subscription = new Subscription($apiKey, $apiSecret);
+$api_data = array(
+    'first_name' => $_POST['first_name'],
+    'last_name' => $_POST['last_name'],
+    'email' => $_POST['email'],
+    'gateway_name' => 'razorpay',
+    'street' => $_POST['street'],
+    'city' => $_POST['city'],
+    'state' => $_POST['state'],
+    'zip_code' => $_POST['zip_code'],
+    'country' => $_POST['country'],
+    'plan_id' => $plan_id,
+);
+
+//Subscribe the plan
+$apiResponse = $subscription->subscribe($api_data);
+
+$customer = $apiResponse->customer;
+$product = $apiResponse->product;
+$plan = $apiResponse->plan;
+$invoice = $apiResponse->invoice;
+
+//Do your payment processor task here
+//After complete the payment process you have to record the payment for the invoice due. Use the following example for that:
+
+try {
+    $invoice_id = $invoice->id;
+    $payment_mode = "Your custom gateway name";
+    $transaction_data = "Which returns by your custom gateway for your record"; //string/object
+    $api_data = $subscription->recordPayment($invoice_id, $payment_mode, $payment_note, $transaction_data);
+
+//Redirct to thank you page, you can also redirect or show your custom thank you page
+    $subscription->redirectThanktyou($api_data->subscription->id, $api_data->subscription->customer_id);
+} catch (Exception $e) {
+    die($e->getMessage());
+}
+?>
---
+++ checkout-with-hostedpage.php
@@ -0,0 +1,40 @@
+<?php
+require('vendor/autoload.php');
+require  __DIR__  .  '/lib/Subscription.php';
+
+if (!isset($_GET['hostedpage']) && !$_GET['hostedpage']) {
+    throw new Exception('Hosted page data is required');
+}
+//Api credential of Pabbly Subscription
+$apiKey = ""; //Put your api key here
+$apiSecret = ""; // Put your api secret here
+
+$hostedpage = $_GET['hostedpage'];
+$subscription = new Subscription($apiKey,$apiSecret);
+//Get hosted page details
+try {
+    $api_data = $subscription->hostedPage($hostedpage);
+} catch (Exception $e) {
+    die($e->getMessage());
+}
+$customer = $api_data->customer;
+$product = $api_data->product;
+$plan = $api_data->plan;
+$invoice = $api_data->invoice;
+
+//Do your payment processor task here
+
+//After complete the payment process you have to record the payment for the invoice due. Use the following example for that:
+
+try {
+$invoice_id = $invoice->id;
+$payment_mode = "Your custom gateway name";
+$transaction_data = "Which returns by your custom gateway for your record"; //string/object
+$api_data  =  $subscription->recordPayment($invoice_id, $payment_mode, $payment_note, $transaction_data);
+
+//Redirct to thank you page
+$subscription->redirectThanktyou($api_data->subscription->id, $api_data->subscription->customer_id);
+} catch (Exception  $e) {
+die($e->getMessage());
+}
+?>
